846b56cc64c3bdb045e38cda957e849056fd6dd4
volekerb <vsevolod.brekelov@gmail.com>
2022-04-13 13:15:10 +0200

    add giphy search

--- src/app.tsx
+++ src/app.tsx
@@ -1,64 +1,58 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 
-import {GiphyFetch} from "@giphy/js-fetch-api";
-import {IGif} from "@giphy/js-types";
+import {useContext} from "react";
+
 import {
-    Gif,
-} from "@giphy/react-components";
-import {useAsync} from "react-async-hook";
-import {useState} from "react";
+    Grid, // our UI Component to display the results
+    SearchBar, // the search bar the user will type into
+    SearchContext, // the context that wraps and connects our components
+    SearchContextManager, // the context manager, includes the Context.Provider
+    SuggestionBar, // an optional UI component that displays trending searches and channel / username results
+} from '@giphy/react-components'
 
-const giphyFetch = new GiphyFetch("u3Ro9JQ5EzjkWOHIN0iNBe5WBA5Q7bOW");
+// the search experience consists of the manager and its child components that use SearchContext
+const SearchExperience = () => (
+    <SearchContextManager apiKey={"u3Ro9JQ5EzjkWOHIN0iNBe5WBA5Q7bOW"}>
+        <Components />
+    </SearchContextManager>
+)
 
-function GifDemo() {
-    const [gif, setGif] = useState<IGif | null>(null);
-    useAsync(async () => {
-        const {data} = await giphyFetch.gif("fpXxIjftmkk9y");
-        setGif(data);
-    }, []);
-    return gif && <Gif gif={gif} width={200}/>;
+// define the components in a separate function so we can
+// use the context hook. You could also use the render props pattern
+const Components = () => {
+    const { fetchGifs, searchKey } = useContext(SearchContext)
+    return (
+        <>
+            <SearchBar />
+            <SuggestionBar />
+            {/**
+             key will recreate the component,
+             this is important for when you change fetchGifs
+             e.g. changing from search term dogs to cats or type gifs to stickers
+             you want to restart the gifs from the beginning and changing a component's key does that
+             **/}
+            <Grid key={searchKey} columns={3} width={800} fetchGifs={fetchGifs} />
+        </>
+    )
 }
 
-async function addSticky() {
-    const stickyNote = await miro.board.createStickyNote({
-        content: 'Hello, World!',
-    });
-
-    await miro.board.viewport.zoomTo(stickyNote);
-}
+// async function addSticky() {
+//     const stickyNote = await miro.board.createStickyNote({
+//         content: 'Hello, World!',
+//     });
+//
+//     await miro.board.viewport.zoomTo(stickyNote);
+// }
 
 function App() {
+
     React.useEffect(() => {
-        addSticky();
+        // addSticky();
     }, []);
 
     return (
-        <div className="grid wrapper">
-            <div className="cs1 ce12">
-                <img src="/src/assets/congratulations.png" alt=""/>
-            </div>
-            <div className="cs1 ce12">
-                <h1>Congratulations!</h1>
-                <p>You've just created your first Miro app!</p>
-                <p>
-                    To explore more and build your own app, see the Miro Developer
-                    Platform documentation.
-                </p>
-            </div>
-            <img src="./logo.gif" width="200" alt="Powered by GIPHY"/>
-            <h4>Gif</h4>
-            <GifDemo/>
-            <div className="cs1 ce12">
-                <a
-                    className="button button-primary"
-                    target="_blank"
-                    href="https://beta.developers.miro.com"
-                >
-                    Read the documentation
-                </a>
-            </div>
-        </div>
+        <SearchExperience/>
     );
 }
 
